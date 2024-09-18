c62f950c61e592055d461829e708673e101727dc
Kanish Kumar <kanishkumar11@outlook.in>
2024-09-18 11:24:33 +0530

    minor update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="text-white flex items-center justify-center text-2xl py-5 gap-3 font-bold shadow-2xl">
-      {/* <div className="h-[35px] w-[35px] object-contain">
+    <Link href="/">
+      <div className="text-white flex items-center justify-center text-2xl py-5 gap-3 font-bold shadow-2xl cursor-pointer">
+        {/* <div className="h-[35px] w-[35px] object-contain">
         <Image src="/1.svg" alt="" width={35} height={35} />
       </div> */}
-      Bezu AI
-    </div>
+        Bezu AI
+      </div>
+    </Link>
   );
 }
