1d3bd8c94b619cbc92aae526d4a2a109640218f6
Kanish Kumar <kanishkumar11@outlook.in>
2024-10-20 19:19:37 +0530

    metatags updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,40 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Bezu AI",
-  description: "Bezu AI",
+  title: "Real-time 1 on 1 video chats with AI Personalities or AI Clones",
+  description: "Our AI can see, listen and speak to you.",
+  keywords:
+    "AI video chat, AI personalities, AI clones, real-time conversations",
+  authors: [{ name: "Bezu Ai" }],
+  creator: "Bezu Ai",
+  publisher: "Bezu Ai",
   openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.mybezu.com/",
+    site_name: "Bezu Ai",
+    title: "Real-time 1 on 1 video chats with AI Personalities or AI Clones",
+    description: "Our AI can see, listen and speak to you. ",
+    images: [
+      {
+        url: "/meta.png",
+        width: 1200,
+        height: 630,
+        alt: "AI Video Chat Platform",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@bezu_ai",
+    creator: "@bezu_ai",
+    title: "Real-time 1 on 1 video chats with AI Personalities or AI Clones",
+    description: "Our AI can see, listen and speak to you. ",
     images: ["/meta.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
   },
 };
 
