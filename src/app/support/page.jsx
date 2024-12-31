5cda58f898b5456f21a50f0fe2fc9a867f6e855f
Kanish Kumar <kanishkumar11@outlook.in>
2024-12-31 15:45:51 +0530

    minor fix

--- src/app/support/page.jsx
+++ src/app/support/page.jsx
@@ -13,7 +13,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { ChevronRight, Loader2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { toast } from "sonner";
